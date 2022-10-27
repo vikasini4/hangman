3b122f8269962c3742ef141722ac3f0324909eb8
vikasini4 <96987281+vikasini4@users.noreply.github.com>
2022-10-27 21:00:31 +0100

    Update milestone_3.py
    updated Docstring

--- milestone_3.py
+++ milestone_3.py
@@ -1,78 +1,109 @@
 # %%
 import random
 
-
 class Game():
+    ''' a Hangman game that asks the player for a letter and checks if it matches in the word. 
+It starts with a default number of lives and a random word from the word list.
+
+Parameters:
+   
+word_list: list - List of words used in the game
+num_lives: int - Number of lives left
+    
+Attributes:
+    
+word: str - The word to be guessed picked randomly from the word_list
+word_guessed: list - A list of the letters of the word, with '_' for each letter not yet guessed
+num_letters: int - The number of unique letters in the word that have not been guessed yet
+num_lives: int - The number of lives left
+list_of_guesses: list - A list of the letters that have already been guessed
+    
+Methods:
+
+check_guess(guess): Checks if the letter is in the word.
+ask_input(): Asks the user for a letter.
+
+'''
+
     def __init__(self,word_list,num_lives):
 
      self.word_list = word_list
      self.num_lives = num_lives
-
      self.word = random.choice(word_list)
-     self.word_guessed = [",", ",", ",", ",", ","]
-     self.num_letters = int(len(set(self.word) - set(self.word_guessed)))
+     self.word_guessed = [","] * len(self.word)
+     self.num_letters = len(list(set(self.word)))
      self.num_lives = 5
      self.word_list = word_list
      self.list_of_guesses =[]
     
     def check_guess(self,guess):
+        """
+        Checks if the guessed letter is in the word.
+        If it is, it will replace the '_' in the word_guessed list with the guessed letter.
+        If it is not, the number of lives will be reduced by 1.
+
+        Parameters:
+
+        guess: str - The guess to be checked
+
+        """
         guess = guess.lower()
-    
-
         if guess in self.word:
-            print("Good guess!", guess, "is in the word")
-            self.word_guessed.extend(guess)
-
+            print(f"Good guess!", guess, "is in the word")
             for i, letter in enumerate(self.word): 
                 if letter == guess:
                     self.word_guessed[i] = guess 
+            print(self.word_guessed)
             self.num_letters -= 1
         else :
-            self.num_lives -= 1
-            
-            print("Sorry,", guess, "is not in the word. Try again.")
-            print("You have", self.num_lives,"lives left." )
-
-            self.list_of_guesses.append(guess)
-
-
+            self.num_lives -= 1 
+            print(f"Sorry,", guess, "is not in the word. Try again.")
+            print(f"You have", self.num_lives,"lives left." )
+       
     def ask_for_input(self):
+        """
+        Asks the user for a guess and checks:
+        1. If the guess has already been tried.
+        2.If the character is a single alphabetic character.
+        If both passes, then it will call the check_guess method.
+
+        """
         while True :
-            guess = input("Enter a single letter")
-            if not guess.isalpha() and not len(guess) == 1:
-                print("Invalid letter. Please, enter a single alphabetic character.")
-            if guess.isnumeric():
-                print("You have entered a number. Please enter a letter.")
+            guess = input("Enter a single letter\t") 
+        
+            if len(guess) != 1:
+                print(f"Invalid input. Please, enter a single alphabetic character.")
+            elif guess.isnumeric():
+                print(f"You have entered a number. Please enter a letter.")
             elif guess in self.list_of_guesses :
-                print("You already tried that letter!")
+                print(f"You already tried that letter!")
             else :
-             self.check_guess(guess)
-             break
+                break
+        self.list_of_guesses.append(guess)
+        self.check_guess(guess) 
 
 def play_game():
     word_list = ["apple", "banana", "pear", "cherry", "strawberry"]
     game = Game(word_list = word_list,num_lives = 5) 
     
-    while True :
-     if game.num_lives == 0:
-            print("You lost")
-            break
+    while True:
+        if game.num_lives > 0:
+         game.ask_for_input()
+         print(f"Guessed letters so far: {', '.join(game.list_of_guesses)}\n")
+        
+        if game.num_lives == 0:
+             print(f"You lost")
+             break
            
-     elif game.num_letters > 0:
-            game.ask_for_input()
-
-     elif game.num_lives  !=0 and game.num_letters==0:
-            print("Congratulations! You won the game.")
-            break
-   
+        elif game.num_lives  !=0 and game.num_letters==0:
+             print(f"Congratulations! You won the game.")
+             break
 
 play_game()
 
 
 
 
-
-
-g# %%
+# %%
 
 # %%
